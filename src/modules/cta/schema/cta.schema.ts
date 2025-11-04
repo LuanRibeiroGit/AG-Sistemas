6bae7847a99fe2896c39b8e37d93f28b6f6bc4e2
LuanRibeiroGit <luancoxa2009@gmail.com>
2025-11-04 11:56:39 -0300

    feat(cta): estruturação do schema

--- src/modules/cta/schema/cta.schema.ts
+++ src/modules/cta/schema/cta.schema.ts
@@ -1,1 +1,22 @@
-export class Cta {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
+
+
+export type CtaDocument = Cta & Document
+
+@Schema({ timestamps: true })
+export class Cta {
+    @Prop({ required: true })
+    name: string
+
+    @Prop({ required: true, unique: true })
+    email: string
+
+    @Prop({ required: true })
+    company: string
+
+    @Prop({ required: true })
+    intention: string
+}
+
+export const CtaSchema = SchemaFactory.createForClass(Cta)
