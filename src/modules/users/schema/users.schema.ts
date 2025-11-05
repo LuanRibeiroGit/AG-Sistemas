c4423328846496eff440c04cb0653c13068f0fbd
LuanRibeiroGit <luancoxa2009@gmail.com>
2025-11-05 18:21:18 -0300

    feat(users): colocando o role no schema

--- src/modules/users/schema/users.schema.ts
+++ src/modules/users/schema/users.schema.ts
@@ -1,29 +1,32 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-import * as bcrypt from 'bcrypt';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
+import * as bcrypt from 'bcrypt'
 
 
-export type UserDocument = User & Document;
+export type UserDocument = User & Document
 
 @Schema({ timestamps: true })
 export class User {
   @Prop({ required: true })
-  name: string;
+  name: string
 
   @Prop({ required: true, unique: true })
-  email: string;
+  email: string
+
+  @Prop({ required: true })
+  role: string
 
   @Prop({ required: true})
-  password: string;
+  password: string
 
 }
 
-export const UserSchema = SchemaFactory.createForClass(User);
+export const UserSchema = SchemaFactory.createForClass(User)
 
 UserSchema.pre<UserDocument>('save', async function (next) {
-  if (!this.isModified('password')) return next();
+  if (!this.isModified('password')) return next()
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
